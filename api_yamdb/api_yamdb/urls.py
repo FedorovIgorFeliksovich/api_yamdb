47a5d005b00b1097ffb445d441bc40fe02e0abe3
FedorovIgorFeliksovich <114092373+FedorovIgorFeliksovich@users.noreply.github.com>
2023-02-03 17:10:30 +0300

    Update urls.py

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,30 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-
-)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path('api/v1/', include('users.urls')),
+    path('api/', include('api.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path('api/token/', 
-        TokenObtainPairView.as_view(), 
-        name='token_obtain_pair'),
-    path('api/token/refresh/', 
-        TokenRefreshView.as_view(), 
-        name='token_refresh'),
-    path('api/token/verify/', 
-        TokenVerifyView.as_view(), 
-        name='token_verify'),
 ]
