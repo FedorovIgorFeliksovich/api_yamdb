26e2279af8f62d12e6cca84d791415aab0a02825
RomanTurundaev <107366488+RomanTurundaev@users.noreply.github.com>
2023-02-04 15:57:15 +0300

    Исправлен Docstring класса NoPutRouter

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -16,15 +16,12 @@ app_name = 'api'
 
 class NoPutRouter(DefaultRouter):
     """
-    Router class that disables the PUT method.
+    Исключаем метод PUT из роутера.
     """
     def get_method_map(self, viewset, method_map):
-
         bound_methods = super().get_method_map(viewset, method_map)
-
         if 'put' in bound_methods.keys():
             del bound_methods['put']
-
         return bound_methods
 
 
